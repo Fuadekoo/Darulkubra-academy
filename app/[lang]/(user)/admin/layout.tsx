88889ecac3557cf14628c304e77d22da25d09c6e
Fuadekoo <fuadabdurahman0859@gmail.com>
2025-05-03 23:12:33 +0300

    feat: refactor layout function signature to improve type definition for children prop

--- app/[lang]/(user)/admin/layout.tsx
+++ app/[lang]/(user)/admin/layout.tsx
@@ -3,8 +3,13 @@ import UserLayout from "@/components/ui/userLayout";
 import { Home } from "lucide-react";
 import { redirect } from "next/navigation";
 import { auth } from "../../../../lib/auth";
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // This layout is for admin only
 
-export default async function layout(children: React.ReactNode) {
   const session = await auth();
 
   if (session?.user?.role !== "admin") redirect(`/en/${session?.user?.role}`);
